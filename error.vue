e9eb367e6ad305ad0cec8bda6c0d7e80df9f450b
Andy Evans <andy.evans@razor.co.uk>
2025-02-15 19:52:14 +0000

    Bit of a rejig

---
+++ error.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+import AppTemplate from './components/AppTemplate.vue';
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+
+const goToHome = () => clearError({ redirect: '/' })
+
+const title = computed(() => props.error?.statusCode === 404 ? 'Page not found' : 'Error')
+
+</script>
+
+<template>
+  <AppTemplate>
+    <div class="error-title">{{ title }}</div>
+    <div>Head back to the home page, you might have more luck there!</div>
+    <button class="home-button" @click="goToHome">Go to Home Page</button>
+  </AppTemplate>
+</template>
+<style scoped>
+.error-title {
+  @apply text-4xl font-bold;
+  @apply mb-3;
+}
+
+.home-button {
+  @apply mt-5;
+}
+</style>
