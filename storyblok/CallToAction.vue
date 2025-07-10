73375ec16e48cf2cbdb9297f0681550954bd725b
Andy Evans <andy.evans@razor.co.uk>
2025-07-10 10:40:56 +0100

    Introduce Biography components

---
+++ storyblok/CallToAction.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button @click="action">{{ props.blok.Text }}</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({ blok: Object })
+
+const action = async () => {
+  await navigateTo(props.blok.Link.cached_url);
+}
+</script>
