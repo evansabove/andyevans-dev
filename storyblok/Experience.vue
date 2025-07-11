2fd14bf3cb702d5777a951c0f3cfc3385cd7606d
Andy Evans <andy.evans@razor.co.uk>
2025-07-11 18:44:36 +0100

    Revised experience component

--- storyblok/Experience.vue
+++ storyblok/Experience.vue
@@ -1,24 +1,37 @@
 <template>
   <div class="experience">
-    <div class="experience-title">
-      {{ props.blok.title }}
-    </div>
-    <div class="experience-business">
-      {{ props.blok.business }}
-    </div>
-    <div class="experience-date">
-      {{ $dayjs(props.blok.start).format("MMM YYYY") }} -
-      {{ props.blok.end ? $dayjs(props.blok.end).format("MMM YYYY") : 'Present' }}
-    </div>
-    <div class="experience-location">
-      📍 {{ props.blok.location }}
+    <div class="experience-header">
+      <div class="experience-logo" v-if="props.blok.logo">
+        <img :src="props.blok.logo.filename" alt="Company Logo" />
+      </div>
+
+      <div class="experience-meta">
+        <div class="experience-title">
+          {{ props.blok.title }}
+        </div>
+
+        <div class="experience-business">
+          {{ props.blok.business }}
+        </div>
+
+        <div class="experience-date">
+          {{ $dayjs(props.blok.start).format("MMM YYYY") }} -
+          {{ props.blok.end ? $dayjs(props.blok.end).format("MMM YYYY") : 'Present' }}
+        </div>
+
+        <div class="experience-location">
+          📍 {{ props.blok.location }}
+        </div>
+      </div>
     </div>
 
-    <div class="experience-tags">
-      <TagList :bloks="props.blok.tags" />
-    </div>
+    <div class="experience-body">
+      <div class="experience-tags">
+        <TagList :bloks="props.blok.tags" />
+      </div>
 
-    <div class="experience-summary" v-html="html" />
+      <div class="experience-summary" v-html="html" />
+    </div>
   </div>
 </template>
 
@@ -31,12 +44,29 @@ const { render } = richTextResolver()
 
 const renderedSections = render(props.blok.summary.content) as []
 const html = renderedSections.join('<br />')
+
 </script>
 
 <style scoped>
 .experience {
   @apply flex flex-col;
   @apply mb-20;
+
+  &-header {
+    @apply flex flex-row;
+    @apply mb-5;
+  }
+
+  &-logo {
+    @apply mr-5;
+    width: 100px;
+    height: 100px;;
+  }
+
+  &-details {
+    @apply flex flex-col;
+    @apply mb-20;
+  }
 
   &-title {
     @apply font-bold;
@@ -49,6 +79,8 @@ const html = renderedSections.join('<br />')
 
   &-tags {
     @apply mt-2;
-  };
+  }
+
+  ;
 }
 </style>
