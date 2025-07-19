304771698425e6ca24a7ee4144c45b60d7cba8b5
Andy Evans <andy.evans@razor.co.uk>
2025-07-19 12:50:32 +0100

    Framework

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <header class="header">
-    <nav>
-      <div class="header-text">
-        <NuxtLink to="/" class="no-underline">{{ config.public.appName }}</NuxtLink>
+    <div class="header-title">
+      <NuxtLink to="/" class="header-link">{{ config.public.appName }}</NuxtLink>
+    </div>
+
+    <nav class="header-nav">
+      <div class="header-nav-links">
+        <NuxtLink to="/posts" class="header-link">Posts</NuxtLink>
+      </div>
+
+      <div class="header-hamburger">
+        <Icon name="fa6-solid:bars" class="header-hamburger-icon" />
       </div>
     </nav>
   </header>
@@ -14,13 +22,44 @@ const config = useRuntimeConfig();
 
 <style scoped>
 .header {
-  @apply bg-purple-900;
-}
-
-.header-text {
   @apply text-white;
-  @apply ps-5 py-5;
-  @apply cursor-pointer;
+  @apply bg-purple-900;
+  @apply flex;
+  @apply px-5 py-5;
   @apply text-xl;
+  @apply gap-10;
+  @apply justify-between md:justify-start;
+
+  &-nav {
+    @apply flex;
+    @apply gap-5;
+
+    &-links {
+      @apply flex;
+      @apply gap-5;
+      @apply hidden md:block;
+    }
+  }
+
+  &-nav-links {
+    @apply gap-5;
+    @apply hidden md:flex;
+  }
+
+  &-divider {
+    @apply hidden md:block;
+  }
+
+  &-link {
+    @apply no-underline;
+  }
+
+  &-hamburger {
+    &-icon {
+      @apply text-2xl;
+      @apply cursor-pointer;
+      @apply md:hidden;
+    }
+  }
 }
 </style>
